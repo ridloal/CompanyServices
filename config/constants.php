ec65f3016334b753116f71edd4c51263a184af9e
Muhammad Ridlo Alimudin <ridlo.alimudin@xtremax.com>
2022-02-12 01:34:11 +0700

    add constants file

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php 
+//describe constants to use globally
+
+return [
+    'voucher' => [
+        'status_available' => '0',
+        'status_locked' => '1',
+        'status_owned' => '2',
+        'status_reedemed' => '3',
+        'status_expired' => '4',
+    ]
+];
