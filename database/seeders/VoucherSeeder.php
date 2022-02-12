9786f931bc9f680fa14e044af6713022df7f4b0b
Muhammad Ridlo Alimudin <ridlo.alimudin@xtremax.com>
2022-02-13 03:35:13 +0700

    Prepare for testing data

---
+++ database/seeders/VoucherSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Voucher;
+
+class VoucherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Voucher::factory()->count(10)->create();
+    }
+}
